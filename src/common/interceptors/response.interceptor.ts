d94181dbba3d280690296da717145794a75eaa9c
Khai-Tran24 <tranmaiquangkhai@gmail.com>
2026-06-08 14:34:45 +0700

    fix: update pagination in response

--- src/common/interceptors/response.interceptor.ts
+++ src/common/interceptors/response.interceptor.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import {
@@ -18,6 +19,7 @@ export class ResponseInterceptor<T> implements NestInterceptor<T, any> {
         statusCode: context.switchToHttp().getResponse().statusCode,
         message: data.message || 'Success',
         data: data.data || data,
+        ...(data.pagination && { pagination: data.pagination }),
       })),
     );
   }
