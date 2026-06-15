f3b6bdea6375f91030286fd29a4dd1ed98798a1a
Khai-Tran24 <tranmaiquangkhai@gmail.com>
2026-06-15 11:22:33 +0700

    Squashed commit of the following:
    commit 44e17caaf55159c9e068f99dc0d6b482ce1dad41
    Author: Khai-Tran24 <tranmaiquangkhai@gmail.com>
    Date:   Mon Jun 15 11:22:10 2026 +0700
    
        fix: number limit and init analytics

--- src/contract/contract.module.ts
+++ src/contract/contract.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ContractController } from './contract.controller';
 import { ContractService } from './contract.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Contract } from './entity/contract.dto';
+import { Contract } from './entity/contract.entity';
 import { UserModule } from 'src/user/user.module';
 
 @Module({
