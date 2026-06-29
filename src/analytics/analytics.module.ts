b63e23e863cbca4d658c93c2c065b7f142997e62
Khai-Tran24 <tranmaiquangkhai@gmail.com>
2026-06-29 08:25:12 +0700

    Squashed commit of the following:
    commit eeb00d7316aa0416848b78114c8447949c5b48bf
    Author: Khai-Tran24 <tranmaiquangkhai@gmail.com>
    Date:   Mon Jun 29 08:20:29 2026 +0700
    
        feat: create get dashboard api and fix user discount price to array
    
    commit 3447a4f8fd59957904f0873a9b5e7c719ef0ed43
    Author: Khai-Tran24 <tranmaiquangkhai@gmail.com>
    Date:   Fri Jun 26 16:31:39 2026 +0700
    
        Squashed commit of the following:
    
        commit 545e5cc5a9529676fe961dce91438215aa3f8c5f
        Author: Khai-Tran24 <tranmaiquangkhai@gmail.com>
        Date:   Fri Jun 26 16:26:52 2026 +0700
    
            fix: can not save time to database
    
        commit 2296daf4f099aa5f9e82a2aabf47e7374f96b00e
        Author: Khai-Tran24 <tranmaiquangkhai@gmail.com>
        Date:   Wed Jun 24 14:30:32 2026 +0700
    
            feat: update code base on new requirement
    
    commit 7895a8e66591cf83943973bb829343e238125b17
    Author: Khai-Tran24 <tranmaiquangkhai@gmail.com>
    Date:   Wed Jun 24 14:34:21 2026 +0700
    
        Squashed commit of the following:
    
        commit 2296daf4f099aa5f9e82a2aabf47e7374f96b00e
        Author: Khai-Tran24 <tranmaiquangkhai@gmail.com>
        Date:   Wed Jun 24 14:30:32 2026 +0700
    
            feat: update code base on new requirement
    
    commit 44e17caaf55159c9e068f99dc0d6b482ce1dad41
    Author: Khai-Tran24 <tranmaiquangkhai@gmail.com>
    Date:   Mon Jun 15 11:22:10 2026 +0700
    
        fix: number limit and init analytics

--- src/analytics/analytics.module.ts
+++ src/analytics/analytics.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AnalyticsService } from './analytics.service';
 import { AnalyticsController } from './analytics.controller';
 import { ContractModule } from 'src/contract/contract.module';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
   providers: [AnalyticsService],
   controllers: [AnalyticsController],
-  imports: [ContractModule],
+  imports: [ContractModule, UserModule],
 })
 export class AnalyticsModule {}
--- src/analytics/analytics.service.ts
+++ src/analytics/analytics.service.ts
@@ -1,7 +1,90 @@
 import { Injectable } from '@nestjs/common';
 import { ContractService } from 'src/contract/contract.service';
+import { UserService } from 'src/user/user.service';
+import { GetAnalyticsDataDto } from './dto/get-analytics-query';
 
 @Injectable()
 export class AnalyticsService {
-  constructor(private readonly contractService: ContractService) {}
+  constructor(
+    private readonly contractService: ContractService,
+    private readonly userService: UserService,
+  ) {}
+
+  private async getSummaryData(
+    query: GetAnalyticsDataDto,
+  ): Promise<SummaryData> {
+    const [contractsSummary, usersSummary] = await Promise.all([
+      this.contractService.computeContractsSummary(query),
+      this.userService.computeUserStatistics(),
+    ]);
+
+    return {
+      contracts: contractsSummary,
+      users: usersSummary,
+    };
+  }
+
+  private async getChartData(query: GetAnalyticsDataDto): Promise<ChartData> {
+    const contractsChartData =
+      await this.contractService.getContractChartData(query);
+
+    return {
+      contractsOverTime: contractsChartData.contractsOverTime,
+      percentageOfContractsByStatus:
+        contractsChartData.percentageOfContractsByStatus,
+      percentageOfContractsByPropertyType:
+        contractsChartData.percentageOfContractsByPropertyType,
+      percentageOfContractsByPaymentStatus:
+        contractsChartData.percentageOfContractsByPaymentStatus,
+    };
+  }
+
+  async getAnalytics(query: GetAnalyticsDataDto): Promise<AnalyticsData> {
+    const [summaryData, chartData] = await Promise.all([
+      this.getSummaryData(query),
+      this.getChartData(query),
+    ]);
+
+    return {
+      summary: summaryData,
+      chart: chartData,
+    };
+  }
+}
+
+interface AnalyticsData {
+  summary: SummaryData;
+  chart: ChartData;
+}
+
+interface ChartData {
+  contractsOverTime: {
+    labels: string[];
+    data: number[];
+  };
+  percentageOfContractsByStatus: {
+    labels: string[];
+    data: number[];
+  };
+  percentageOfContractsByPropertyType: {
+    labels: string[];
+    data: number[];
+  };
+  percentageOfContractsByPaymentStatus: {
+    labels: string[];
+    data: number[];
+  };
+}
+
+interface SummaryData {
+  contracts: {
+    totalContracts: number;
+    contractsByStatus: Record<string, number>;
+    contractsByPropertyType: Record<string, number>;
+    contractsByPaymentStatus: Record<string, number>;
+  };
+  users: {
+    activeUsers: number;
+    inactiveUsers: number;
+  };
 }
